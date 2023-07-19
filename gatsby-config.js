11e297c1ef11a00e5575bc8f11c214102fa33906
Vikram Kumar <vicky1996p@gmail.com>
2023-07-19 18:21:52 +0530

    my first gatsby project

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,3 +7,20 @@ module.exports = {
   },
   plugins: [],
 }
+module.exports = {
+  siteMetadata: {
+    title: "My First Gatsby Site",
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+    "gatsby-plugin-mdx",
+  ],
+};
---
+++ src/components/layout.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link, useStaticQuery, graphql } from "gatsby";
+import "./layout.css";
+
+const Layout = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+  return (
+    <div className="container">
+      <header className="site-title">{data.site.siteMetadata.title}</header>
+      <nav>
+        <ul className="nav-links">
+          <li className="nav-link-item">
+            <Link to="/" className="nav-link-text">
+              Home
+            </Link>
+          </li>
+          <li className="nav-link-item">
+            <Link to="/about" className="nav-link-text">
+              About
+            </Link>
+          </li>
+          <li className="nav-link-item">
+            <Link to="/Blog" className="nav-link-text">
+              Blog
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h1 className="heading">{pageTitle}</h1>
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
